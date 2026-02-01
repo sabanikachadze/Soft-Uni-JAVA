ee512563d12a8be6bc99529f6b339fb5ece14853
Saba Nikachadze <nikachadzesaba027@gmail.com>
2026-02-01 19:02:43 +0400

    FOreignLanguages console program

---
+++ src/Basic/Syntax/ForeignLanguages.java
@@ -0,0 +1,21 @@
+package Basic.Syntax;
+
+import java.util.Scanner;
+
+public class ForeignLanguages {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read input
+        String language = scanner.nextLine();
+        // Determine output
+        switch (language) {
+            case "USA":
+            case "England": System.out.println("English"); break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico": System.out.println("Spanish"); break;
+            default: System.out.println("unknown"); break;
+        }
+    }
+}
