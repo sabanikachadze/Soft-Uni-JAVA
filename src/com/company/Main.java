a7680b7adf44bc18df54afba01f4f7e2d5693181
Saba Nikachadze <nikachadzesaba027@gmail.com>
2026-02-02 14:20:00 +0400

    numbers 1-100 divisible by 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,27 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int month = scanner.nextInt();
-
-        switch(month){
-            case 1:
-                System.out.println("1");
-                break;
-            case 2:
-                System.out.println("2");
-                break;
-            case 3:
-                System.out.println("3");
-                break;
-            case 4:
-                System.out.println("4");
-                break;
-            case 5:
-                System.out.println("5");
-                break;
-            default:
-                System.out.println("Nothing");
-                break;
-        }
+        System.out.println("Hello SoftUni");
     }
 }
