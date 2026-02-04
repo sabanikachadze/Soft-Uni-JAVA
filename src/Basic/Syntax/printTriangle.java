0e1535f503c8bfc678a63569fdaa962f12d585f3
Saba Nikachadze <nikachadzesaba027@gmail.com>
2026-02-04 23:58:03 +0400

    Print triangle.

---
+++ src/Basic/Syntax/printTriangle.java
@@ -0,0 +1,30 @@
+package Basic.Syntax;
+
+import java.util.Scanner;
+
+public class printTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (scanner.hasNextInt()) {
+            printTriangle(scanner.nextInt());
+        }
+    }
+
+    static void printTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            printLine(i);
+        }
+
+        for (int i = size - 1; i >= 1; i--) {
+            printLine( i);
+        }
+    }
+
+    static void printLine(int end) {
+        for (int i = 1; i <= end; i++) {
+            System.out.print(i + (i == end ? "" : " "));
+        }
+        System.out.println();
+    }
+}
